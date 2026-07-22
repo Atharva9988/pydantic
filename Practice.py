f66c6ed80668a7b79db9978537b894dc073aaf75
Atharva9988 <atharvathorve2@gmail.com>
2026-07-22 06:42:48 +0530

    22/07/2026

--- Practice.py
+++ Practice.py
@@ -1,15 +1,16 @@
-from pydantic import BaseModel
-from typing import List, Dict
+from pydantic import BaseModel, EmailStr
+from typing import List, Dict, Optional
 
 class Player(BaseModel):
 
     name: str
+    email:EmailStr
     age: int
     game: str
     role: str
     weight: float
     playing_status: bool
-    injuries: List[str]
+    injuries: Optional[List[str]] = None
     contact_details: Dict[str,str]
 
 def insert_player_data (player:Player):
@@ -17,11 +18,13 @@ def insert_player_data (player:Player):
     print(player.name)
     print(player.age)
     print(player.game)
+    print(player.injuries)
     print('inserted')
 
 def update_player_data (player:Player):
 
     print(player.name)
+    print(player.email)
     print(player.age)
     print(player.game)
     print(player.role)
@@ -31,7 +34,7 @@ def update_player_data (player:Player):
     print(player.contact_details)
     print('Inserted')
 
-player_info= {'name':'Atharva', 'age':20, 'game': 'Cricket', 'role':'Lefthand Batsman and Righthand Offspin Bowler', 'weight':70.5, 'playing_status':True, 'injuries':['calf','muscle pull'], 'contact_details':{'profile':'Atharva.being_private', 'phone':'9828489796'}}
+player_info= {'name':'Atharva','email': 'yuv@gmail.com', 'age':20, 'game': 'Cricket', 'role':'Lefthand Batsman and Righthand Offspin Bowler', 'weight':70.5, 'playing_status':True, 'injuries':['calf','muscle pull'], 'contact_details':{'profile':'Atharva.being_private', 'phone':'9828489796'}}
 
 player1 = Player(**player_info)
 
