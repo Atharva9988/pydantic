3344cedaf1268bac5ac86160b33e51c003142b45
Atharva9988 <atharvathorve2@gmail.com>
2026-07-24 07:38:17 +0530

    24/07/2026

--- Practice.py
+++ Practice.py
@@ -1,17 +1,17 @@
 from pydantic import BaseModel, EmailStr, AnyUrl, Field
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Annotated
 
 class Player(BaseModel):
 
-    name: str
+    name: Annotated[str, Field(max_length=50, title='Name of the patient', description='give the name of the patient in less than 50 chars', examples=['Shreyash','Harshal'])] 
     email:EmailStr
     linkedin: AnyUrl
-    age: int
+    age: int = Field(gt=0, lt=120)
     game: str
     role: str
-    weight: float
-    playing_status: bool
-    injuries: Optional[List[str]] = None
+    weight: float =Field(gt=0)
+    playing_status: Annotated[bool, Field(default=None, description='Is the player playing today')]
+    injuries: Annotated[Optional[List[str]], Field(default=None, max_length=5)]
     contact_details: Dict[str,str]
 
 def insert_player_data (player:Player):
@@ -36,7 +36,7 @@ def update_player_data (player:Player):
     print(player.contact_details)
     print('Inserted')
 
-player_info= {'name':'Atharva','email': 'yuv@gmail.com', 'age':20, 'game': 'Cricket', 'role':'Lefthand Batsman and Righthand Offspin Bowler', 'weight':70.5, 'playing_status':True, 'injuries':['calf','muscle pull'], 'contact_details':{'profile':'Atharva.being_private', 'phone':'9828489796'}}
+player_info= {'name':'Atharva','email': 'shreyash@gmail.com','linkedin':'http://linkedin.com', 'age':20, 'game': 'Cricket', 'role':'Lefthand Batsman and Righthand Offspin Bowler', 'weight':70.5, 'playing_status':True, 'injuries':['calf','muscle pull'], 'contact_details':{'profile':'Atharva.being_private', 'phone':'9828489796'}}
 
 player1 = Player(**player_info)
 
