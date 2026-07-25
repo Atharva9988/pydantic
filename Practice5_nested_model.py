c548187fafb655b8918a1a47a0e056cbda6c10d9
Atharva9988 <atharvathorve2@gmail.com>
2026-07-25 23:31:45 +0530

    25/07/2026

---
+++ Practice5_nested_model.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+class Address(BaseModel):
+    city: str
+    district: str
+    pin: str
+
+
+class Player(BaseModel):
+
+    name: str
+    gender: str
+    age: int
+    address: Address
+
+address_dict = {'city': 'Karjat', 'district':'Raigad', 'pin': '410203'}
+
+address1 = Address(**address_dict)
+
+player_dict = {'name':'Atharva', 'gender': 'male', 'age':20, 'address': address1}
+
+player1 = Player(**player_dict)
+
+print(player1)
+print(player1.name)
---
+++ Practice6_serialization.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+class Address(BaseModel):
+    city: str
+    district: str
+    pin: str
+
+
+class Player(BaseModel):
+
+    name: str
+    gender: str
+    age: int
+    address: Address
+
+address_dict = {'city': 'Karjat', 'district':'Raigad', 'pin': '410203'}
+
+address1 = Address(**address_dict)
+
+player_dict = {'name':'Atharva', 'gender': 'male', 'age':20, 'address': address1}
+
+player1 = Player(**player_dict)
+
+player1.model_dump()
+
+temp = player1.model_dump(exclude={'address':['district']})
+
+print(temp)
+print(type(temp))
